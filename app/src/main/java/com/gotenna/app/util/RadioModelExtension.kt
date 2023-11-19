0f6798589d913bc4b56ba90e487a39a68b4a5cdb
rene-gotenna <114943487+rene-gotenna@users.noreply.github.com>
2023-11-19 17:54:02 -0500

    Fix Compilation Issue with Duplicate Ext Functions

--- app/src/main/java/com/gotenna/app/util/RadioModelExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.gotenna.app.util
-
-import com.gotenna.radio.sdk.common.models.radio.RadioModel
-import com.gotenna.radio.sdk.common.models.radio.RadioState
-
-fun RadioModel.isConnected() = state == RadioState.CONNECTED
-
-fun RadioModel.isScannedOrDisconnected() = state == RadioState.SCANNED || state == RadioState.DISCONNECTED
